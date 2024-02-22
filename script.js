ac1b13241572649a0e7b9044c7e7392aa0a4a785
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-02-22 15:31:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const getSum = () => {
 	});
 	const totalRow = document.createElement('tr');
 	total.innerHTML = `<td colspan ="2"> Total price : ${total.toFixed(2)}</td>`
-   document.getElementByTagName('table').appendChild(totalRow);
+   document.getElementsByTagName('table').appendChild(totalRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
