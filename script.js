5288a097bdfd2fb14c5f9fda746e407a5cd81087
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-02-22 11:02:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  let total =0;
+	let price = document.querySlectorAll(.'price');
+	price.forEach(prices =>{
+		total += parseInt(prices.textContent);
+	});
+	const total = Document.createElement('tr');
+	total.innerHTML = `<td colspan ="2"> Total price : ${total.fixed(2)}</td>`
+   document.getElementByTagName('table').appendChild(total);
 };
 
 getSumBtn.addEventListener("click", getSum);
